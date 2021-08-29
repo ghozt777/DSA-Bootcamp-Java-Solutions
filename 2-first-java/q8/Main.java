72ebca8af47122a93f99b75a85002c13fd503e6f
ghozt777 <ghozt777.code@gmail.com>
2021-08-29 11:02:07 +0530

    added solution for q8

---
+++ 2-first-java/q8/Main.java
@@ -0,0 +1,20 @@
+/*
+    Program that takes rupees as input from the user
+    and calculates its equivalent value in USD
+*/
+
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter amount in ₹ : ");
+        int rupees = in.nextInt();
+        double dollars = getDollarBillsBaby(rupees);
+        System.out.println("₹ "+rupees+" -> "+" $ "+dollars);
+    }
+    static double getDollarBillsBaby(int rupees){
+        return 0.014*rupees;
+        // this is as of today's date may not ne vaalid when the program is running
+    }
+}
